332b2341edf6da99d3fca5076f0cc7bf022328d0
Dev Tsubaki <dev.tsubaki@gmail.com>
2013-09-24 23:38:43 +0900

    initialize program

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Method1();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void Method1()
+        {
+            try
+            {
+                Method2();
+            }
+            catch (Exception ex)
+            {
+                throw;
+                //throw ex;
+            }
+        }
+
+        static void Method2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
